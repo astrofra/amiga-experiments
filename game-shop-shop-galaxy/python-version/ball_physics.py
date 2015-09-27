a32f1b4e48876f57b9f521aebd72dc2db307758a
astrofra <astrofra@gmail.com>
2015-09-27 18:06:44 +0200

    - Draft physics & 2D projection.

--- game-shop-shop-galaxy/python-version/ball_physics.py
+++ game-shop-shop-galaxy/python-version/ball_physics.py
@@ -1,18 +1,45 @@
 import math
 
 velocity_x = 0.0
-velocity_y = 0.0
+velocity_z = 0.0
 
 pos_x = 0.0
-pos_y = 0.0
+pos_z = 0.0
 
 radius = 1.0
 
-field_width = 10.0
-field_length = 30.0
+board_width = 10.0
+board_length = 30.0
 
 def reset():
 	pos_x = 0.0
-	pos_y = 0.0
+	pos_z = 0.0
 	velocity_x = 0.0
-	velocity_y = 0.0
+	velocity_z = 0.0
+
+def applyImpulse(x,z):
+	global velocity_x, velocity_z
+	velocity_x = x
+	velocity_z = z
+
+def update(dt):
+	global pos_x, pos_z, velocity_x, velocity_z, board_width, board_length
+	pos_x += velocity_x * dt
+	pos_z += velocity_z * dt
+
+	# basic dynamics & collision
+	if pos_x > board_width * 0.5:
+		pos_x = board_width * 0.5
+		velocity_x *= -1
+	else:
+		if pos_x < board_width * -0.5:
+			pos_x = board_width * -0.5
+			velocity_x *= -1
+
+	if pos_z > board_length * 0.5:
+		pos_z = board_length * 0.5
+		velocity_z *= -1
+	else:
+		if pos_z < board_length * -0.5:
+			pos_z = board_length * -0.5
+			velocity_z *= -1
--- game-shop-shop-galaxy/python-version/main.py
+++ game-shop-shop-galaxy/python-version/main.py
@@ -4,6 +4,9 @@ import gs.plus.input as input
 
 import ball_physics as ball
 
+def project3DTo2D():
+	return ball.pos_x, ball.pos_z, 0.5
+
 SCR_PHYSIC_WIDTH = 320
 SCR_PHYSIC_HEIGHT = 200
 
@@ -17,8 +20,16 @@ render.init(SCR_DISP_WIDTH, SCR_DISP_HEIGHT, "pkg.core")
 # provide access to the data folder
 gs.MountFileDriver(gs.StdFileDriver("assets/"), "@assets/")
 
+ball.reset()
+ball.applyImpulse(10.0, 10.0)
+
 while not input.key_press(gs.InputDevice.KeyEscape):
+	scr_margin_x = (SCR_DISP_WIDTH - (SCR_PHYSIC_WIDTH * SCR_SCALE_FACTOR)) / 2.0
+	ball.update(1.0 / 60.0)
+	ball_2d_x, ball_2d_y, ball_2d_scale = project3DTo2D()
+
 	render.clear()
-	render.image2d((SCR_DISP_WIDTH - (SCR_PHYSIC_WIDTH * SCR_SCALE_FACTOR)) / 2.0, 0, SCR_SCALE_FACTOR, "@assets/game_board.png")
-	render.image2d((SCR_DISP_WIDTH - (SCR_PHYSIC_WIDTH * SCR_SCALE_FACTOR)) / 2.0, SCR_DISP_HEIGHT - (32 * SCR_SCALE_FACTOR), SCR_SCALE_FACTOR, "@assets/game_score_panel.png")
+	render.image2d(scr_margin_x, 0, SCR_SCALE_FACTOR, "@assets/game_board.png")
+	render.image2d(scr_margin_x, SCR_DISP_HEIGHT - (32 * SCR_SCALE_FACTOR), SCR_SCALE_FACTOR, "@assets/game_score_panel.png")
+	render.sprite2d(scr_margin_x + ball_2d_x, ball_2d_y, 36 * SCR_SCALE_FACTOR * ball_2d_scale, "@assets/game_ball.png")
 	render.flip()
