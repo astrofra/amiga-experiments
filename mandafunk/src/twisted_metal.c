7a4ea260d2f6fbe926d360b9365916dd572683a3
astrofra <astrofra@gmail.com>
2016-02-08 23:37:12 +0100

    - Draft & ugly twister.

--- mandafunk/src/twisted_metal.c
+++ mandafunk/src/twisted_metal.c
@@ -4,17 +4,28 @@
 void twistedMetal(void)
 {
 	u16 vramIndex = TILE_USERINDEX;
+	u16 scroll_jump_table[512];
+	u16 i, vcount = 0;
+
+	static void hBlank(){
+		// VDP_setVerticalScroll(PLAN_A, scroll_jump_table[GET_VCOUNTER]);
+		VDP_setVerticalScroll(PLAN_A, scroll_jump_table[(GET_VCOUNTER + vcount) & 511] + ((vcount << 2) & 255));
+	}
+
+	for(i = 0; i < 512; i++)
+		scroll_jump_table[i] = (i * (sinFix16(i << 1) + 64)) >> 8;
 
 	SYS_disableInts();
 
 	/* Set a larger tileplan to be able to scroll */
-	VDP_setPlanSize(64, 64);
+	VDP_setPlanSize(32, 128);
 
 	VDP_clearPlan(APLAN, 0);
 	VDP_clearPlan(BPLAN, 0);
 
 	/* Load the fond tiles */
-	VDP_drawImageEx(APLAN, &twister, TILE_ATTR_FULL(PAL0, FALSE, FALSE, FALSE, vramIndex), 0, 32, FALSE, FALSE);
+	VDP_drawImageEx(APLAN, &twister, TILE_ATTR_FULL(PAL0, FALSE, FALSE, FALSE, vramIndex), 0, 0, FALSE, FALSE);
+	VDP_drawImageEx(APLAN, &twister, TILE_ATTR_FULL(PAL0, FALSE, FALSE, FALSE, vramIndex), 0, 320 / 8, FALSE, FALSE);
 	vramIndex += twister.tileset->numTile;
 
 	VDP_setPalette(PAL0, twister.palette->data);
@@ -23,8 +34,12 @@ void twistedMetal(void)
 
 	SYS_enableInts();
 
+	VDP_setHInterrupt(1);
+	SYS_setHIntCallback(&hBlank);
+
 	while (TRUE)
 	{
 		VDP_waitVSync();
+		vcount++;
 	}	
 }
