2db4e3774910cf5c7d61b8086184a405952d377f
astrofra <astrofra@gmail.com>
2016-03-20 22:34:18 +0100

    - Amos demo :  * working music.  * first screen draft.

--- amos-demo-python-remake/src/amos_demo.py
+++ amos-demo-python-remake/src/amos_demo.py
@@ -14,20 +14,50 @@
 
 import os
 import gs
+import gs.plus.render as render
+import gs.plus.input as input
+import gs.plus.clock as clock
+import math
 import time
+from utils import *
+
+demo_screen_size = [1280, 720]
+amiga_screen_size = [320, 256]
+zoom_size = demo_screen_size[1] / amiga_screen_size[1]
 
 gs.LoadPlugins(gs.get_default_plugins_path())
+render.init(demo_screen_size[0], demo_screen_size[1], "pkg.core")
 
 # mount the system file driver
-gs.MountFileDriver(gs.StdFileDriver())
+gs.MountFileDriver(gs.StdFileDriver("assets/"), "@assets/")
 
-# create an OpenAL mixer and wrap it with the MixerAsync interface
-al = gs.ALMixer()
-al.Open()
-channel = al.Stream(os.path.join(os.getcwd(), "assets/amos_demo_music.xm"))
+al = None
+channel = None
 
-# wait until the user decides to exit the program or the stream ends
-print("Playing on channel %d, press Ctrl+C to stop." % channel)
+def play_music():
+	global al, channel
+	# create an OpenAL mixer and wrap it with the MixerAsync interface
+	al = gs.ALMixer()
+	al.Open()
+	channel = al.Stream("@assets/amos_demo_music.xm")
 
-while al.GetPlayState(channel) == gs.MixerPlaying:
-	time.sleep(0.1)
+def render_title_page():
+	global al, channel
+	fx_timer = 0.0
+	fx_duration = math.pi
+	while True:
+		dt_sec = clock.update()
+		fx_timer += dt_sec
+		render.clear()
+		y_damping = RangeAdjust(fx_timer, 0.0, fx_duration, 1.0, 0.0)
+		y_damping = Clamp(y_damping, 0.0, 1.0)
+		y_damping = EaseInOutQuick(y_damping)
+		y_scr = abs(math.sin((fx_timer + 1.2) * 4.0)) * y_damping
+		render.image2d((demo_screen_size[0] - amiga_screen_size[0] * zoom_size) * 0.5, y_scr * demo_screen_size[1], zoom_size, "@assets/titlepage.png")
+		al.GetPlayState(channel)
+		render.flip()
+
+
+play_music()
+
+render_title_page()
