3764c7b36cf5a6bffc2e42c53ac0df415f5b9881
astrofra <astrofra@gmail.com>
2016-03-22 23:11:47 +0100

    - Amos demo :  * new amiga fonts.  * new text writer to emulate the original MIDDLE() procedure.

--- amos-demo-python-remake/src/amos_demo.py
+++ amos-demo-python-remake/src/amos_demo.py
@@ -19,7 +19,7 @@ import math
 from utils import *
 
 demo_screen_size = [1280, 720]
-amiga_screen_size = [320, 256]
+amiga_screen_size = [320, 220]
 zoom_size = demo_screen_size[1] / amiga_screen_size[1]
 
 gs.LoadPlugins(gs.get_default_plugins_path())
@@ -46,20 +46,47 @@ def update_music():
 
 
 def render_credits():
-	strings = [["THE AMOS DEMO",25,1,1],
-	           ["PROGRAMMING BY",60,1,0],
-	           ["Peter Hickman",75,1,0],
-	           ["MUSIC BY",105,1,0],
-	           ["Allister Brimble",120,1,0]]
+	strings = [["THE AMOS DEMO", 25, 1, 1, "dustismo-roman-bold", 42],
+			["PROGRAMMING BY", 60, 1, 0 ,"dustismo-roman-bold", 42],
+			["Peter Hickman", 75, 1, 0, "dustismo-roman-bold", 42],
+			["MUSIC BY", 105, 1, 0, "dustismo-roman-bold", 42],
+			["Allister Brimble", 120, 1, 0, "dustismo-roman-bold", 42],
+			["IF YOU WOULD LIKE SOME CONTROL", 155, 1, 0, "amiga4ever-pro2", 18],
+			["OVER THIS DEMO PRESS SPACE NOW", 165, 1, 0, "amiga4ever-pro2", 18],
+			["There is an IMPORTANT text file", 185, 1,0, "amiga4ever-pro2", 18],
+			["on this disk, please read it.", 195, 1,0, "amiga4ever-pro2", 18]]
+	render_text_screen(strings)
 
+
+def render_text_screen(strings = None, fade = 1.0):
+
+	if strings is None:
+		return
+
+	sys = render.get_render_system()
+	render.get_renderer().EnableBlending(True)
+
+	fonts_dict = {}
 	fx_timer = 0.0
 	while fx_timer < 4.0:
 		dt_sec = clock.update()
 		fx_timer += dt_sec
 		render.clear()
 		for line in strings:
-			render.text2d(0, (256 - line[1]) * zoom_size, line[0], 30, font_path="@assets/topaz-new.ttf")
+			font_key = line[4] + "_" + str(line[5])
+			if not font_key in fonts_dict:
+				fonts_dict[font_key] = gs.RasterFont("@assets/" + line[4] + ".ttf", line[5], 512)
+
+			rect = fonts_dict[font_key].GetTextRect(sys, line[0])
+			x = (demo_screen_size[0] - rect.GetWidth()) * 0.5
+			y = (amiga_screen_size[1] - line[1]) * zoom_size
+			fonts_dict[font_key].Write(sys, line[0], gs.Vector3(x, y, 0.5))
+
+		sys.DrawRasterFontBatch()
+		update_music()
 		render.flip()
+
+	render.get_renderer().EnableBlending(False)
 
 
 def render_title_page_bouncing():
@@ -88,7 +115,7 @@ def render_title_page_still():
 		render.flip()
 
 
-render_credits()
 render_title_page_bouncing()
 play_music()
 render_title_page_still()
+render_credits()
