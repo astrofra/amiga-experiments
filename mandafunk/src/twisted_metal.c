d7afe00a66b110074413d27ddc06cb96991e4025
astrofra <astrofra@gmail.com>
2016-06-29 09:14:31 +0900

    - Mandafunk : tweaked the twister.

--- mandafunk/src/twisted_metal.c
+++ mandafunk/src/twisted_metal.c
@@ -6,10 +6,15 @@
 #define CST_DATA_PORT 0xC00000
 #define CST_WRITE_VSRAM_ADDR(adr)   ((0x4000 + ((adr) & 0x3FFF)) << 16) + (((adr) >> 14) | 0x10)
 
+s16 scroll_jump_table[TWISTER_TABLE_SIZE];
+s16 hscroll_jump_table[TWISTER_TABLE_SIZE];
+s16 hblank_table[TWISTER_TABLE_SIZE];
+s16 hscroll_table[256];
+
+
 void twistedMetal(void)
 {
 	u16 vramIndex = TILE_USERINDEX;
-	u16 scroll_jump_table[TWISTER_TABLE_SIZE];
 	u16 i, vcount = 0;
 
     vu16 *pw;
@@ -22,19 +27,31 @@ void twistedMetal(void)
 		// VDP_setVerticalScroll(PLAN_A, scroll_jump_table[(GET_VCOUNTER + vcount) & ((TWISTER_TABLE_SIZE >> 2) - 1)] + (64 + sinFix16(vcount << 1)) << 1);
 
 	    *pl = CST_WRITE_VSRAM_ADDR(0);
-	    *pw = scroll_jump_table[(GET_VCOUNTER + vcount) & ((TWISTER_TABLE_SIZE >> 2) - 1)] + (64 + sinFix16(vcount << 1)) << 1;		
+	    *pw = scroll_jump_table[(GET_VCOUNTER + vcount) & ((TWISTER_TABLE_SIZE >> 2) - 1)] + (64 + sinFix16(vcount << 1)) << 1;
+	}
+
+	static void vBlank(){
+		vcount = (vcount + 1) & 511;
+		VDP_setHorizontalScrollLine(PLAN_A, 0, hscroll_table, 224, TRUE);
 	}
 
 	for(i = 0; i < TWISTER_TABLE_SIZE; i++)
+	{
 		scroll_jump_table[i] = (i * (sinFix16(i << 2) + 64)) >> 7;
+		hscroll_jump_table[i] = (sinFix16(i << 2) + 64) >> 2;
+	}
+
+	// for(i = 0; i < TWISTER_TABLE_SIZE; i++)
+	// 	hblank_table[i] = (64 + sinFix16(i << 1)) << 1;
 
 	SYS_disableInts();
 
 	/* Set a larger tileplan to be able to scroll */
-	VDP_setPlanSize(32, 64);
+	VDP_setPlanSize(64, 64);
 
 	VDP_clearPlan(APLAN, 0);
 	VDP_clearPlan(BPLAN, 0);
+	VDP_setScrollingMode(HSCROLL_LINE, VSCROLL_PLANE);
 
 	/* Load the fond tiles */
 	VDP_drawImageEx(APLAN, &twister, TILE_ATTR_FULL(PAL0, FALSE, FALSE, FALSE, vramIndex), 8, 0, FALSE, FALSE);
@@ -49,10 +66,12 @@ void twistedMetal(void)
 
 	VDP_setHInterrupt(1);
 	SYS_setHIntCallback(&hBlank);
+	SYS_setVIntCallback(&vBlank);
 
 	while (TRUE)
 	{
-		VDP_waitVSync();
-		vcount++;
+		// for(i = 0; i < 256; i++)
+			hscroll_table[i] = hscroll_jump_table[i + vcount]; // (cosFix16(i << 1) >> 1);
+			i = (i + 1) & 0xFF;
 	}	
 }
